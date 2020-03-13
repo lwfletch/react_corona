2146a0a0bc729d2dcb0afdd0b9112eb24ee19164
Trey Fletcher <trey.fletcher@linuxacademy.com>
2020-03-13 00:34:14 -0400

    Updates

---
+++ precache-manifest.c59243b76210892682c26cdd2c960bbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b32b1a5fb4d5d5ad7ecdeb11231de48",
+    "url": "/react_corona/index.html"
+  },
+  {
+    "revision": "792d2a86788c2dac6a45",
+    "url": "/react_corona/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3532de42ebc2c2c3bb5a",
+    "url": "/react_corona/static/js/2.f633f1a8.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/react_corona/static/js/2.f633f1a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792d2a86788c2dac6a45",
+    "url": "/react_corona/static/js/main.a35a9b15.chunk.js"
+  },
+  {
+    "revision": "1cd68437f8a7d663b4c9",
+    "url": "/react_corona/static/js/runtime-main.c2d3493a.js"
+  }
+]);
